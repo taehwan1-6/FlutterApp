83c89bd363b964fe0cfa7d87afc0622c83b55065
kantae <thwan97@gmail.com>
2022-01-09 23:59:35 +0900

    ch04_1_2 제목 줄 추가하기 - 1단계, Scaffold에 appBar 선언

---
+++ material_flutter_app/lib/ch04/ch04_1_2.dart
@@ -0,0 +1,41 @@
+// ch04. 플러터 위젯 사용법
+// ch04-1. 스캐폴드를 이용한 머티리얼 디자인
+// ch04-1-2. 제목 줄 추가하기
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material Flutter App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MaterialFlutterApp(),
+    );
+  }
+}
+
+class MaterialFlutterApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MaterialFlutterApp();
+  }
+}
+
+class _MaterialFlutterApp extends State<MaterialFlutterApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Material Design App'),),
+    );
+  }
+}
