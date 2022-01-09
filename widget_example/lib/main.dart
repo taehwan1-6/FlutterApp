94f582432d04e32233704ac7fc526fc9d21d7138
kantae <thwan97@gmail.com>
2022-01-10 02:42:14 +0900

    ch04-3-1 덧셈 계산기 앱 만들기 - 1단계 : StatefulWidget 만들기

--- widget_example/lib/main.dart
+++ widget_example/lib/main.dart
@@ -1,34 +1,22 @@
 // ch04 플러터 위젯 사용법
 // ch04-3 사용자와 상호작용하는 앱 만들기
-// ch04-3-1 덧셈 계산기 앱 만들기
+// ch04-3-1 덧셈 계산기 앱 만들
 
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp( MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  static const String _title = 'Widget Example';
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: _title,
+      home: WidgetApp()
     );
   }
 }
@@ -52,6 +40,6 @@ class _WidgetExampleState extends State<WidgetApp> {
           ),
         ),
       ),
-    )
+    );
   }
 }
