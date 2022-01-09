0b4254d55aec88f678a71ad7c022b39b2b239141
kantae <thwan97@gmail.com>
2022-01-10 01:56:03 +0900

    ch04-2 이미지와 폰트 추가하기, imageWidget 파일 생성, image폴더에 lion사진 넣기, pubspec.yaml파일에 assets: 설정하기

---
+++ image_font_add_app/lib/imageWidget.dart
@@ -0,0 +1,19 @@
+// 04. 플러터 위젯 사용법
+// 04-2. 이미지와 폰트 추가하기
+// 04_2_1. 이미지 표시하기
+
+import 'package:flutter/material.dart';
+
+class ImageWidgetApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _ImageWidgetApp();
+  }
+}
+
+class _ImageWidgetApp extends State<ImageWidgetApp> {
+  @override
+  Widget build(BuildContext context) {
+    return null;
+  }
+}
