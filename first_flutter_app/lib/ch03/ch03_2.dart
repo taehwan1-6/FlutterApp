afacb229679be93dbd0fbb3b0a1e615619e9856a
kantae <thwan97@gmail.com>
2022-01-09 19:30:46 +0900

    ch03-2 생명주기 순서 출력하기 - 1단계, createState() 함수 출력

---
+++ first_flutter_app/lib/ch03/ch03_2.dart
@@ -0,0 +1,56 @@
+// ch03-2 생명주기 순서 출력하기
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp( MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    print('createState');
+    return _MyApp();
+  }
+}
+
+class _MyApp extends State<StatefulWidget> {
+  var switchValue = false;
+  String test = "hello";
+  Color _color = Colors.blue;
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      darkTheme: ThemeData.light(),
+      home: Scaffold(
+        body: Center(
+          child: ElevatedButton(
+            child: Text('$test'),
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all(_color)),
+            onPressed: () {
+              if (_color == Colors.blue) {
+                setState(() {
+                  test = 'flutter';
+                  _color = Colors.amber;
+                });
+              } else {
+                setState(() {
+                  test = 'flutter';
+                  _color = Colors.blue;
+                });
+              }
+            },
+          ),
+        ),
+      ),
+
+    );
+  }
+}
