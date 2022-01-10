69e231f99e63b407e59f6f1b86363f6adf623703
kantae <thwan97@gmail.com>
2022-01-11 04:26:22 +0900

    ch05-2-3 동물을 추가하는 화면 만들기 - 1단계: secondPage.dart 파일 수정

--- listview_example/lib/sub/secondPage.dart
+++ listview_example/lib/sub/secondPage.dart
@@ -1,30 +1,26 @@
 import 'package:flutter/material.dart';
 import '../animalItem.dart';
 
-class SecondApp extends StatelessWidget{
-  final List<Animal> list;
-  SecondApp({Key? key, required this.list}) : super(key: key);
+class SecondApp extends StatefulWidget{
 
+  @override
+  State<StatefulWidget> createState() => _SecondApp();
+  List<Animal>? list;
+  SecondApp({Key? key, @required this.list}) : super(key: key);
+
+}
+
+class _SecondApp extends State<SecondApp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
         child: Center(
-          child: ListView.builder(itemBuilder: (context, position) {
-            return Card( // 이 부분에 위젯을 이용해 데이터를 표시
-              child: Row(
-                children: <Widget>[
-                  Image.asset(
-                    list![position].imagePath!,
-                    height: 100,
-                    width: 100,
-                    fit: BoxFit.contain,),
-                  Text(list![position].animalName!),
-                ],
-              ),
-            );
-          },
-          itemCount: list!.length
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[ // 위젯 담기
+
+            ],
           ),
         ),
       ),
