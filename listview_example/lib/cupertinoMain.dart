ecec4bdfce2ceb11e08fc8c2356fbea300a8be08
kantae <thwan97@gmail.com>
2022-01-11 15:44:39 +0900

    ch06-1-3 쿠퍼티노 리스트뷰 만들기 준비

--- listview_example/lib/cupertinoMain.dart
+++ listview_example/lib/cupertinoMain.dart
@@ -1,6 +1,7 @@
 // ch06. ios 스타일로 플러터 앱 만들기
 
 import 'package:flutter/cupertino.dart';
+import 'animalItem.dart';
 
 class CupertinoMain extends StatefulWidget {
   @override
@@ -11,6 +12,7 @@ class CupertinoMain extends StatefulWidget {
 
 class _CupertinoMain extends State<CupertinoMain> {
   CupertinoTabBar? tabBar;
+  List<Animal> animalList = List.empty(growable: true);
 
   @override
   void initState() {
@@ -19,6 +21,16 @@ class _CupertinoMain extends State<CupertinoMain> {
       BottomNavigationBarItem(icon: Icon(CupertinoIcons.home)),
       BottomNavigationBarItem(icon: Icon(CupertinoIcons.add)),
     ]);
+
+    animalList.add(Animal(animalName: "벌", kind: "곤충", imagePath: "repo/images/bee.png"));
+    animalList.add(Animal(animalName: "고양이", kind: "포유류", imagePath: "repo/images/cat.png"));
+    animalList.add(Animal(animalName: "젖소", kind: "포유류", imagePath: "repo/images/cow.png"));
+    animalList.add(Animal(animalName: "강아지", kind: "포유류", imagePath: "repo/images/dog.png"));
+    animalList.add(Animal(animalName: "여우", kind: "포유류", imagePath: "repo/images/fox.png"));
+    animalList.add(Animal(animalName: "원숭이", kind: "영장류", imagePath: "repo/images/monkey.png"));
+    animalList.add(Animal(animalName: "돼지", kind: "포유류", imagePath: "repo/images/pig.png"));
+    animalList.add(Animal(animalName: "늑대", kind: "포유류", imagePath: "repo/images/wolf.png"));
+
   }
 
   @override
