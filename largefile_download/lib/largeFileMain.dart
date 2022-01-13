ec8cb95dea000b9cf5ebe61aff6774f84970390a
kantae <thwan97@gmail.com>
2022-01-14 04:24:59 +0900

    ch07-2-2 URL을 직접 입력해 내려받기 - 1단계: 텍스트필드 사용

--- largefile_download/lib/largeFileMain.dart
+++ largefile_download/lib/largeFileMain.dart
@@ -10,19 +10,29 @@ class LargeFileMain extends StatefulWidget {
 
 class _LargeFileMain extends State<LargeFileMain> {
   // 내려받을 이미지 주소
-  final imgUrl =
-      'https://images.pexels.com/photos/240040/pexels-photo-240040.jpeg'
-      '?auto=compress';
+  TextEditingController? _editingController;
   bool downloading = false; // 지금 내려받는 중인지 확인하는 변수
   var progressString = ""; // 현재 얼마나 내려받았는지 표시하는 변수
   String file = ""; // 내려받은 파일
 
+  @override
+  void initState() {
+    super.initState();
+    _editingController = new TextEditingController(
+        text: 'https://images.pexels.com/photos/240040/pexels-photo-240040.jpeg'
+            '?auto=compress');
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Large File Example'),
+        title: TextField(
+          controller: _editingController,
+          style: TextStyle(color: Colors.white),
+          keyboardType: TextInputType.text,
+          decoration: InputDecoration(hintText: 'url 입력하세요'),
+        ),
       ),
       body: Center(
         child: downloading
