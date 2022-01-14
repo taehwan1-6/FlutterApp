baedcb820da6d72840c6fbab60adb3e0d2f1030d
kantae <thwan97@gmail.com>
2022-01-14 19:26:19 +0900

    ch09-2-2 좋아하는 과일 표시하기 - 파일읽기 - 3단계: readListFile() 함수 만들기

--- fileio_example/lib/fileApp.dart
+++ fileio_example/lib/fileApp.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'dart:io';
 import 'package:path_provider/path_provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class FileApp extends StatefulWidget {
   @override
@@ -10,6 +11,35 @@ class FileApp extends StatefulWidget {
 class _FileApp extends State<FileApp> {
   int _count = 0;
 
+
+  Future<List<String>> readListFile() async {
+    List<String> itemList = new List.empty(growable: true);
+    var key = 'first';
+    SharedPreferences pref = await SharedPreferences.getInstance();
+    bool? firstCheck = pref.getBool(key);
+    var dir = await getApplicationDocumentsDirectory();
+    bool fileExist = await File(dir.path + '/fruit.txt').exists();
+
+    if (firstCheck == null || firstCheck == false || fileExist == false) {
+      pref.setBool(key, true);
+      var file = await DefaultAssetBundle.of(context).loadString('repo/fruit.txt');
+      File(dir.path + '/fruit.txt').writeAsStringSync(file);
+      var array = file.split('\n');
+      for (var item in array) {
+        print(item);
+        itemList.add(item);
+      }
+      return itemList;
+    } else {
+      var file = await File(dir.path + '/fruit.txt').readAsString();
+      var array = file.split('\n');
+      for (var item in array) {
+        print(item);
+        itemList.add(item);
+      }
+      return itemList;
+    }
+  }
 
   @override
   void initState() {
