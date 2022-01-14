e6882eb838f3a0f34753145f1954f15bdb6fbcea
kantae <thwan97@gmail.com>
2022-01-15 03:00:42 +0900

    ch10-1-1 SQLite 사용하기 - 7단계: AddTodoApp 클래스 작성

---
+++ sql_example/lib/addTodo.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:sqflite/sqlite_api.dart';
+import 'todo.dart';
+
+class AddTodoApp extends StatefulWidget {
+  final Future<Database> db;
+  AddTodoApp(this.db)
+
+  @override
+  State<StatefulWidget> createState() => _AddTodoApp();
+}
+
+class _AddTodoApp extends State<AddTodoApp> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Todo 추가'),
+      ),
+      body: Container(
+        child: Center(
+          child: Column(
+            children: <Widget>[],
+          ),
+        ),
+      ),
+    );
+  }
+}
