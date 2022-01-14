750d99222307d450c720db814e4a4ac8ce55ceda
kantae <thwan97@gmail.com>
2022-01-14 16:48:22 +0900

    ch08-2 할 일을 기록하는 앱 만들기 - 1단계: subDetail.dart 파일 만들기

---
+++ todo_list/lib/subDetail.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SubDetail extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _SubDetail();
+}
+
+class _SubDetail extends State<SubDetail> {
+  @override
+  Widget build(BuildContext context) {
+    return null;
+  }
+}
