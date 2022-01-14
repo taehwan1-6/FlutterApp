876a11c97ca329c49b058c256fcf61f9bdff4c6c
kantae <thwan97@gmail.com>
2022-01-14 20:52:58 +0900

    ch09-2-4 내려받은 이미지를 로고로 사용하기 - 2단계: 로고바꾸기 버튼 만들기

--- intro_example/lib/main.dart
+++ intro_example/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'largeFileMain.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,18 +15,34 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('로고바꾸기'),
+        actions: <Widget>[
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).push(MaterialPageRoute(builder: (context) => LargeFileMain()));
+            },
+            child: Text(
+              '로고 바꾸기',
+              style: TextStyle(color: Colors.white),
+            )),
+        ],
+      ),
+      body: Container(),
+    );
+  }
 }
