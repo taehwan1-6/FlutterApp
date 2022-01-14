7362be188fc26c1ae4d887b6705524bbd5c09406
kantae <thwan97@gmail.com>
2022-01-14 17:17:57 +0900

    ch08-2-2 할 일 보기 기능 만들기기 - 1단계: todolist에 목록 추가해놓기

--- todo_list/lib/subDetail.dart
+++ todo_list/lib/subDetail.dart
@@ -6,6 +6,18 @@ class SubDetail extends StatefulWidget {
 }
 
 class _SubDetail extends State<SubDetail> {
+  List<String> todoList = new List.empty(growable: true);
+
+
+  @override
+  void initState() {
+    super.initState();
+    todoList.add('당근 사오기');
+    todoList.add('약 사오기');
+    todoList.add('청소하기');
+    todoList.add('부모님께 전화하기');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
