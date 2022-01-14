34f3e7e89569323775365f6891fdaaeb46e2552e
kantae <thwan97@gmail.com>
2022-01-14 18:05:24 +0900

    ch08-2-3 할 일 추가 기능 만들기 - 3단계: 텍스트필드 입력 -> 버튼 누름 -> 데이터 전달 후 현재화면 종료

--- todo_list/lib/secondDetail.dart
+++ todo_list/lib/secondDetail.dart
@@ -3,17 +3,27 @@ import 'package:flutter/material.dart';
 class SecondDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    TextEditingController controller = new TextEditingController();
+
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Second Page'),
       ),
       body: Container(
         child: Center(
-          child: ElevatedButton(
-            onPressed: () {
-              Navigator.of(context).pushReplacementNamed('/third');
-            },
-            child: Text('세 번째 페이지로 이동하기'),
+          child: Column(
+            children: <Widget>[
+              TextField(
+                controller: controller,
+                keyboardType: TextInputType.text,
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).pop(controller.value.text);
+                  },
+                  child: Text('저장하기'))
+            ],
           ),
         ),
       ),
