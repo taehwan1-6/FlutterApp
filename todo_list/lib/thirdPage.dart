40b5c16d385b0a9b873ab84ea3a0192a19c1f55c
kantae <thwan97@gmail.com>
2022-01-14 17:03:56 +0900

    ch08-2 할 일을 기록하는 앱 만들기 - 5단계: thirdPage.dart 파일 생성

---
+++ todo_list/lib/thirdPage.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class ThirdDetail extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Third Page'),
+      ),
+      body: Container(
+        child: Center(
+          child: ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: Text('첫 번째 페이지로 돌아가기'),
+          ),
+        ),
+      ),
+    );
+  }
+}
