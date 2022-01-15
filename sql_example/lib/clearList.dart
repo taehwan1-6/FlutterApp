be583e7abc1a876e23b9d8da5259049b96ab375a
kantae <thwan97@gmail.com>
2022-01-15 14:29:26 +0900

    ch10-3 질의문으로 추가 기능 만들기: 완료한 일만 모아서 보기 - 검색질의 - 5단계: 완료한일만 불러오는 getClearList() 함수 만들기

--- sql_example/lib/clearList.dart
+++ sql_example/lib/clearList.dart
@@ -19,4 +19,18 @@ class _ClearListApp extends State<ClearListApp> {
       ),
     );
   }
+
+  Future<List<Todo>> getClearList() async {
+    final Database database = await widget.database;
+    List<Map<String, dynamic>> maps = await database
+      .rawQuery('select title, content, id from todos where active=1');
+
+    return List.generate(maps.length, (i) {
+      return Todo(
+        title: maps[i]['title'].toString(),
+        content: maps[i]['content'].toString(),
+        id: maps[i]['id'],);
+    });
+  }
+
 }
