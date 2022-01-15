90b8e93f2b06bd65f17632f1b3f6b3a44dec9d13
kantae <thwan97@gmail.com>
2022-01-15 13:07:45 +0900

    ch10-2 데이터베이스에서 데이터 처리하기 - 데이터 수정하기 - 5단계: onTap안에 텍스트필드 넣고 일정도 수정

--- sql_example/lib/main.dart
+++ sql_example/lib/main.dart
@@ -100,18 +100,22 @@ class _DatabaseApp extends State<DatabaseApp> {
                             ),
                           ),
                           onTap: () async {
+                            TextEditingController controller = new TextEditingController(text: todo.content);
+
                             Todo result = await showDialog(
                               context: context,
                               builder: (BuildContext context) {
                                 return AlertDialog(
                                   title: Text('${todo.id} : ${todo.title}'),
-                                  content: Text('Todo를 체크하겠습니까?'),
+                                  content: TextField(
+                                    controller: controller,
+                                    keyboardType: TextInputType.text,
+                                  ),
                                   actions: <Widget>[
                                     TextButton(
                                       onPressed: () {
-                                        setState(() {
-                                          todo.active == 1 ? todo.active = 0 : todo.active = 1;
-                                        });
+                                        todo.active == 1 ? todo.active = 0 : todo.active = 1;
+                                        todo.content = controller.value.text;
                                         Navigator.of(context).pop(todo);
                                       },
                                       child: Text('예')),
