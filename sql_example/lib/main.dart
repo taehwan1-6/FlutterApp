025ee7a49d33ee4e0bb25fe15e32ac476eb76fc6
kantae <thwan97@gmail.com>
2022-01-15 14:18:15 +0900

    ch10-3 질의문으로 추가 기능 만들기: 완료한 일만 모아서 보기 - 검색질의 - 3단계: main에 clearList.dart 라우트

--- sql_example/lib/main.dart
+++ sql_example/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
+import 'clearList.dart';
 import 'addTodo.dart';
 import 'todo.dart';
 
@@ -24,7 +25,8 @@ class MyApp extends StatelessWidget {
       initialRoute: '/',
       routes: {
         '/': (context) => DatabaseApp(database),
-        '/add': (context) => AddTodoApp(database)
+        '/add': (context) => AddTodoApp(database),
+        '/clear': (context) => ClearListApp(database),
       },
     );
   }
