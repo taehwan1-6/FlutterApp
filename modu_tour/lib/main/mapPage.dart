1bb22174005324500a8b7aad6e2ecac28cfeb727
kantae <thwan97@gmail.com>
2022-01-19 13:10:07 +0900

    ch15 모두의 여행 앱 - 메인(main)화면 만들기, 1단계: 메인화면 기본골격 만들기

---
+++ modu_tour/lib/main/mapPage.dart
@@ -0,0 +1,17 @@
+// 메인 화면에서 관광지 목록 구성 (관광 전보 오픈 API 연동)면
+
+import 'package:flutter/material.dart';
+
+class MapPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MapPage();
+  }
+}
+
+class _MapPage extends State<MapPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ modu_tour/lib/main/settingPage.dart
@@ -0,0 +1,17 @@
+// 설정 화면
+
+import 'package:flutter/material.dart';
+
+class SettingPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _SettingPage();
+  }
+}
+
+class _SettingPage extends State<SettingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
