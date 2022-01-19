022ebd59717df447d0fbf4ee514abff73f4cebfa
kantae <thwan97@gmail.com>
2022-01-19 12:47:22 +0900

    ch14 모두의 여행 앱 - 안돼서 다시 수정 (gradle바꾸고, 패키지 수정)

--- modu_tour/lib/main.dart
+++ modu_tour/lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:google_mobile_ads/google_mobile_ads.dart';
+
 import 'package:modu_tour/signPage.dart';
 import 'login.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  MobileAds.instance.initialize();
+
   runApp(MyApp());
 }
 
