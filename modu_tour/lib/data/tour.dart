3b0efad67c83661c610780c7539fd301b45fb6ff
kantae <thwan97@gmail.com>
2022-01-20 18:33:51 +0900

    ch15 모두의 여행 앱 - 관광정보 API이용해 데이터 불러오기, 1단계: TourData 클래스 작성

---
+++ modu_tour/lib/data/tour.dart
@@ -0,0 +1,51 @@
+// tour.dart : 광광지 데이터 구성
+
+class TourData {
+  String? title;
+  String? tel;
+  String? zipcode;
+  String? address;
+  var id;
+  var mapx;
+  var mapy;
+  String? imagePath;
+
+  TourData(
+    {
+      this.id,
+      this.title,
+      this.tel,
+      this.zipcode,
+      this.address,
+      this.mapx,
+      this.mapy,
+      this.imagePath;
+    }
+  );
+
+  TourData.fromJson(Map data)
+    : id = data['contentid'],
+      title = data['title'],
+      tel = data['tel'],
+      zipcode = data['zipcode'],
+      address = data['add1'],
+      mapx = data['mapx'],
+      mapy = data['mapy'],
+      imagePath = data['firstimage'];
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'title': title,
+      'tel': tel,
+      'zipcode': zipcode,
+      'address': address,
+      'mapx': mapx,
+      'mapy': mapy,
+      'imagePath': imagePath,
+    };
+  }
+
+
+
+}
