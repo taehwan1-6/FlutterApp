25f26574488a3254522318b9af0027ac5df78f1e
kantae <thwan97@gmail.com>
2022-01-21 17:46:25 +0900

    ch15-4 모두의 여행 앱 - 설정 화면 만들기, 1단계: 설정화면 + 푸시알림 수신 여부 설정(공유설정에 저장 SharedPreferences)

--- modu_tour/lib/main/settingPage.dart
+++ modu_tour/lib/main/settingPage.dart
@@ -1,17 +1,124 @@
 // 설정 화면
 
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SettingPage extends StatefulWidget {
+  final DatabaseReference? databaseReference;
+  final String? id;
+  
+  SettingPage({this.databaseReference, this.id});
+  
   @override
-  State<StatefulWidget> createState() {
-    return _SettingPage();
-  }
+  State<StatefulWidget> createState() => _SettingPage();
 }
 
 class _SettingPage extends State<SettingPage> {
+  bool pushCheck = true;
+  
+  @override
+  void initState() {
+    super.initState();
+    _loadData();
+  }
+  
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('설정하기'),
+      ),
+      body: Container(
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Text(
+                    '푸시 알림',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  Switch(
+                    value: pushCheck, 
+                    onChanged: (value) {
+                      setState(() {
+                        pushCheck = value;
+                      });
+                      _setData(value);
+                    })
+                ],
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).pushNamedAndRemoveUntil('/', (Route<dynamic> route) => false);
+                }, 
+                child: Text('로그아웃', style: TextStyle(fontSize: 20),)),
+              SizedBox(
+                height: 50,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  AlertDialog dialog = new AlertDialog(
+                    title: Text('아이디 삭제'),
+                    content: Text('아이디를 삭제하시겠습니까?'),
+                    actions: <Widget>[
+                      TextButton(
+                        onPressed: () {
+                          print(widget.id);
+                          widget.databaseReference!
+                            .child('user')
+                            .child(widget.id!)
+                            .remove();
+                          Navigator.of(context).pushNamedAndRemoveUntil('/', (Route<dynamic> route) => false);
+                        },
+                        child: Text('예')),
+                      TextButton(
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                        child: Text('아니요')),
+                    ],
+                  );
+                 showDialog(
+                   context: context,
+                   builder: (context) {
+                   return dialog;
+                 });
+                }, 
+                child: Text('회원 탈퇴', style: TextStyle(fontSize: 20),)),
+            ],
+            mainAxisAlignment: MainAxisAlignment.center,
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _setData(bool value) async {
+    var key = "push";
+    SharedPreferences pref = await SharedPreferences.getInstance();
+    pref.setBool(key, value);
+  }
+
+  void _loadData() async {
+    var key = "push";
+    SharedPreferences pref = await SharedPreferences.getInstance();
+    setState(() {
+      var value = pref.getBool(key);
+      if (value == null) {
+        setState(() {
+          pushCheck = true;
+        });
+      } else {
+        setState(() {
+          pushCheck = value;
+        });
+      }
+    });
   }
 }
